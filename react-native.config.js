0a27e1b7857d17e33be8ffcfd3ecac71ac3a87bd
Ryan Skinner <hello@ryanskinner.com>
2025-03-05 20:37:33 -0500

    chore: configure Android platform settings

--- react-native.config.js
+++ react-native.config.js
@@ -2,5 +2,16 @@
  * @type {import('@react-native-community/cli-types').UserDependencyConfig}
  */
 module.exports = {
-  dependency: {},
+  dependency: {
+    platforms: {
+      /**
+       * @type {import('@react-native-community/cli-types').AndroidDependencyParams}
+       */
+      android: {
+        sourceDir: './android',
+        packageImportPath: 'import com.shiki.ShikiPackage;',
+        packageInstance: 'new ShikiPackage()',
+      },
+    },
+  },
 }
